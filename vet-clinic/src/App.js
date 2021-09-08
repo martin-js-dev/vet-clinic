d36236257aaf24d22a1f1da6e69d4d2422bdda62
Your Name <you@example.com>
2021-09-08 18:52:14 +0200

    init

--- vet-clinic/src/App.js
+++ vet-clinic/src/App.js
@@ -1,10 +1,11 @@
-
+import Navigation from './components/Header/Navigation';
 import './App.css';
+import './index.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navigation></Navigation>
     </div>
   );
 }
